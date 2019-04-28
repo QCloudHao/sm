f5442508eaf97e7c2f0cd054b1d50eef46bbff18
QCloudHao <499930858@qq.com>
2019-04-28 14:10:34 +0800

    完成登录及个人中心模块

--- sm_service/src/main/java/com/imooc/sm/dao/SelfDao.java
+++ sm_service/src/main/java/com/imooc/sm/dao/SelfDao.java
@@ -1,7 +1,12 @@
 package com.imooc.sm.dao;
 
+import com.imooc.sm.entity.Staff;
+import org.springframework.stereotype.Repository;
+
 /**
  * Created by qyh on 2019/4/28.
  */
+@Repository("selfDao")
 public interface SelfDao {
+    Staff selectByAccount(String account);
 }
--- sm_service/src/main/java/com/imooc/sm/service/SelfService.java
+++ sm_service/src/main/java/com/imooc/sm/service/SelfService.java
@@ -1,7 +1,11 @@
 package com.imooc.sm.service;
 
+import com.imooc.sm.entity.Staff;
+
 /**
  * Created by qyh on 2019/4/28.
  */
 public interface SelfService {
+    Staff login(String account, String password);
+    void changePassword(Integer id, String password);
 }
